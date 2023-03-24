91cebe41cbed1e8ce7e39eeba8d53672e78e4e23
Peter Mwansa <mwansapeter800@gmail.com>
2023-03-25 02:07:42 +0300

    Add code explaining the OOP Concerpts

--- code.js
+++ code.js
@@ -1,18 +1,219 @@
 
-//FUNCTIONAL PROGRAMMING
+// ============ OOP Paradigm =============
 
 
-// scope 
-var car = "Toyota Hilux";     // this function is a global variable, it can be used anywhere in this scope/file
+class Italy {
+    constructor(area, group, teacher) {
+        this.area = area
+        this.group = group
+        this.teacher = teacher
+    }
+}
 
-function bestCars() {
-    var first = "Ferrari";
-    var second = "Lamborghini";      // this varriable is scoped to this function and is referred to as a local variable
-    var third = "Bughatti";
+class France {
+    constructor(area, group, teacher){
+        this.area = area
+        this.group = group
+        this.teacher = teacher
+    }
+}
+
+class School {
+    constructor(area, italyGroup, franceGroup) {
+        this.age = area
+        this.Italy = new Italy(45, italyGroup)
+        this.France = new France(70, franceGroup)
+    }
+}
+
+var SunSchool = new School("1.5 - 7", "middle", "nursery")
+
+console.log(SunSchool.Italy);
+console.log(SunSchool.France);
+
+
+
+
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+class HighSpeedTrain extends Train {
+    constructor(passengers, highSpeedOn, color, lightsOn) {
+        super(color, lightsOn);
+        this.passengers = passengers;
+        this.highSpeedOn = highSpeedOn;
+    }
+    toggleHighSpeed() {
+        this.highSpeedOn = !this.highSpeedOn;
+        console.log('High speed status:', this.highSpeedOn);
+    }
+    toggleLights() {
+        super.toggleLights();
+        super.lightsStatus();
+        console.log('Lights are 100% operational.');
+    }
+}
+
+var myFirstTrain = new Train('red', false);
+console.log(myFirstTrain); // Train {color: 'red', lightsOn: false}
+var mySecondTrain = new Train('blue', false);
+var myThirdTrain = new Train('blue', false);
+console.log(myThirdTrain);
+
+var train4 = new Train('red', false);
+var lights = train4.toggleLights(); // undefined
+train4.lightsStatus(); // Lights on? true
+mySecondTrain.getSelf(); // Train {color: 'red', lightsOn: true}
+train4.getPrototype(); // {constructor: f, toggleLights: f, ligthsStatus: f, getSelf: f, getPrototype: f}
+
+var train5 = new Train('blue', false);
+var highSpeed1 = new HighSpeedTrain(200, false, 'green', false);
+
+train5.toggleLights(); // undefined
+train5.lightsStatus(); // Lights on? true
+highSpeed1.toggleLights(); // Lights on? true, Lights are 100% operational.
+
+
+
+//constructor functions === they are in-built
+
+//e.g Date, Math, Object
+//you can also define custom constractors as in the example below
+
+function Icecream(flavor) {
+    this.flavor = flavor;
+    this.meltIt = function() {
+        console.log(`The ${this.flavor} icecream has melted`);
+    }
 }
 
 
+let kiwiIcecream = new Icecream("kiwi");
+let appleIcecream = new Icecream("apple");
+var kiwi = kiwiIcecream.meltIt(); // --> Icecream {flavor: 'kiwi', meltIt: ƒ}
+var apple = appleIcecream.meltIt();
+console.log(kiwi);
+console.log(apple);
+appleIcecream; // --> Icecream {flavor: 'apple', meltIt: ƒ}
+
+
+
+
+
+
+// //inheritance 
+// class Animal { /* ...class code here... */ }
+// class Bird extends Animal { /* ...class code here... */ }
+// class Eagle extends Bird { /* ...class code here... */ }
+
+//encapsulation 
+//it deals with making a code implementation hidden 
+
+
+//Polymorphism
+//it is a concept that explains that, one method can be used to perform different tasks depending on the input that is manipuilating it.
+class Bird {
+    useWings() {
+        console.log("Flying!")
+    }
+}
+class Eagle extends Bird {
+    useWings() {
+        super.useWings()
+        console.log("Barely flapping!")
+    }
+}
+class Penguin extends Bird {
+    useWings() {
+        console.log("Diving!")
+    }
+}
+var baldEagle = new Eagle();
+var kingPenguin = new Penguin();
+baldEagle.useWings(); // "Flying! Barely flapping!"
+kingPenguin.useWings(); // "Diving!"
+
+
+
+var purchase1 = {
+    shoes: 200,
+    stateTax: 1.2,
+    totalPrice: function() {
+        var calculate = this.shoes * this.stateTax;
+        console.log("The total price for the first purchase is: $" + calculate);
+    }
+}
+purchase1.totalPrice()
+
+
+var purchase1 = {
+    shoes: 133,
+    stateTax: 1.2,
+    totalPrice: function() {
+        var calculate = this.shoes * this.stateTax;
+        console.log("The total price for the second purchase is: $" + calculate);
+    }
+}
+purchase1.totalPrice()
+
+
+
+
+// var shoes = 100;
+// var stateTax = 1.2;
+
+// function totalPrice(shoes, tax) {
+//     return shoes * tax
+// }
+
+// var toPay = totalPrice(shoes, stateTax);
+
+// console.log("You will pay the total of $" + toPay + " for the shoes");
+
+
+
+
+
 //recursion 
+
+
+function factorial(n) {   //the factorial
+    if (n === 0) {
+        return 1;
+    } else {
+        return  n * factorial(n - 1)
+    }
+}
+console.log(factorial(5))
+
+function recurse(x) {
+    if (x > 0 && x < 40) {
+        console.log(x);
+        recurse(x - 1);
+    }
+}
+
+recurse(30);
+
+
+
 var counter = 3;
 
 function myDay() {
@@ -28,6 +229,137 @@ function myDay() {
 }
 
 console.log(myDay());
+
+
+// ===============================================================      PROGRAMMING ASSIGNMENT ================================== 
+// // Task 1: Build a function-based console log message generator
+// var style;
+// var message;
+
+// function consoleStyler(color, background, fontSize, txt) {
+//     message = "%c" + txt;
+//     style = `color: ${color};`
+//     style += `background: ${background};`
+//     style += `font-size: ${fontSize};`
+
+//     console.log(message, style);
+
+    
+// }
+
+// // Task 2: Build another console log message generator
+// function celebrateStyler(reason) {
+//     var fontStyle = "color: tomato; font-size: 50px";
+//     if (reason == "birthday") {
+//         console.log(`%cHappy birthday`, fontStyle);
+//     } else if (reason == "champions") {
+//         console.log(`%cCongrats on the title!`, fontStyle);
+//     } else {
+//         console.log(message, style);
+//     }
+// }
+
+// // Task 3: Run both the consoleStyler and the celebrateStyler functions
+
+// consoleStyler('#1d5c63', '#ede6db', '40px', 'Congrats!');
+// celebrateStyler('birthday');
+
+
+// // Task 4: Insert a congratulatory and custom message
+// function styleAndCelebrate(color, background, fontSize, txt, reason) {
+//     consoleStyler(color, background, fontSize, txt);  
+//     celebrateStyler(reason);
+// }
+
+// // Call styleAndCelebrate
+
+// styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it!', "champions!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNCTIONAL PROGRAMMING
+
+//1) first-class functions == in JS, a function is just another value that we can(pass to another function, save in a var, return from other functions)
+
+function addTwoNums(a, b) {
+    console.log(a + b)
+}
+
+function randomNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+function specificNum() { return 42 };
+
+var useRandom = true;
+
+var getNumber;
+
+if(useRandom) {
+    getNumber = randomNum
+} else {
+    getNumber = specificNum
+}
+
+addTwoNums(getNumber(), getNumber())
+
+
+
+
+// 2) Higher-order functions == accepts other functions as arguments, returns functions when invoked.
+
+function addTwoNums(getNumber1, getNumber2) {
+    console.log(getNumber1() + getNumber2());
+}
+
+
+//3) pure functions == returns the exact same result as long as given the same values.
+
+function addTwoNums(a, b) {
+    console.log(a + b)
+}
+
+
+// oop 
+//creating an object
+var virtualPet = {
+    sleepy: true,
+    nap: function() {
+        this.sleepy = false
+    }
+}
+console.log(virtualPet.sleepy) // true
+virtualPet.nap()
+console.log(virtualPet.sleepy) // false
+
+
+
+// scope 
+var car = "Toyota Hilux";     // this function is a global variable, it can be used anywhere in this scope/file
+
+function bestCars() {
+    var first = "Ferrari";
+    var second = "Lamborghini";      // this varriable is scoped to this function and is referred to as a local variable
+    var third = "Bughatti";
+}
+
+
+
+
+
 
 
 // this function will return the val value and also the function call without undefined. It is like calling the console.log 
