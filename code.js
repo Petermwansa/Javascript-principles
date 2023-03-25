26d5dfa494ebe6e8f462aade078ef270aeadaa5c
Peter Mwansa <mwansapeter800@gmail.com>
2023-03-26 02:44:12 +0300

    Add more code on OOP on classes and a file to demostrate inheritance

--- code.js
+++ code.js
@@ -1,6 +1,37 @@
 
 // ============ OOP Paradigm =============
 
+//designing an OO program
+
+
+//using defalut parameters with classes  
+class WithDefaultParams {
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result of the class with default parameters is:", bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"
+    }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
+
+
+
+//default parameters 
+function withDefaultParameter(number = 5) {
+    console.log("The result of the default parameter function is: ", number * number);
+}
+withDefaultParameter();
+
 
 class Italy {
     constructor(area, group, teacher) {
